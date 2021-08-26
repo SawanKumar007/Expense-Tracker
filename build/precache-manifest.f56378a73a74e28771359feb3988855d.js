6cd15010611bb67168ca70ccbc9ccf138e50514b
Sawan Kumar <88134243+SawanKumar007@users.noreply.github.com>
2021-08-26 18:13:21 +0530

    Add files via upload

---
+++ build/precache-manifest.f56378a73a74e28771359feb3988855d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a2619dd556c6301242f2d835d46884",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7903468a9a3589251c50",
+    "url": "/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7903468a9a3589251c50",
+    "url": "/static/js/main.8ccc2a53.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
